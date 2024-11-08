fab460275628fe0d58e66796407013ff304240b8
Austin Colt <austin@jcolt.net>
2024-11-08 00:23:02 -0700

    Add my own greeting for testing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+// I want to find a way to essentially return a string!
+char* greeting(char* userName) {
+	char suffix[] = "Hello, ";
+	char* finalOut = (char*)malloc(strlen(suffix) + strlen(userName) + 1);
+	
+	strcpy(finalOut, suffix);
+	strcat(finalOut, userName);
+	return finalOut;
+}
 
 int main() {
-    printf("Hello, World!\n");
-	printf("This is a test!\n");
-    return 0;
+	char* introduction = greeting("Austin");
+	printf("%s\n", introduction);
+	free(introduction);
+
+	return 0;
 }
 
